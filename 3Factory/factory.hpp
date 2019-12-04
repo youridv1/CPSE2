219b84cbbeab8d52e62eaf49d99b18aada449b37
youri <youridv1@gmail.com>
2019-12-04 13:02:21 +0100

    update

--- 3Factory/factory.hpp
+++ 3Factory/factory.hpp
@@ -1,7 +1,53 @@
 #ifndef FACTORY_HPP
 #define FACTORY_HPP
 
+#include "exception.hpp"
 #include <fstream>
 #include <memory>
+
+std::ifstream & operator>>( std::ifstream & input, sf::Color & rhs){
+    std::string s;
+    input >> s;
+    const struct {const char* name; sf::Color color; } colors[]
+    {
+        { "yellow", sf::Color::Yellow },
+		{ "red",    sf::Color::Red },
+		{ "green",  sf::Color::Green },
+		{ "blue",   sf::Color::Blue },
+		{ "black",  sf::Color::Black },
+		{ "white",  sf::Color::White }
+    };
+    for( auto const & color : colors ){
+        if(color.name == s){
+            rhs = color.color;
+            return input;
+        }
+    }
+    if(s == ""){ throw end_of_file(); }
+    throw unknown_color( s );    
+}
+
+std::ifstream & operator>>(std::ifstream & input, sf::Vector2f & rhs ){
+    char c;
+    if( ! ( input >> c )){ throw end_of_file(); }
+    if( c != '(' ){ throw invalid_position( c ); }
+    if( ! ( input >> rhs.x )){ throw end_of_file(); }
+    if( ! ( input >> c )){ throw end_of_file(); }
+    if( ! ( input >> rhs.y )){ throw end_of_file(); }
+    if( ! ( input >> c )){ throw end_of_file(); }
+    if( c != ')' ){ throw invalid_position( c ); }
+    return input;
+}
+
+std::unique_ptr<drawable> drawable_read(std::ifstream & input){
+    sf::Vector2f position;
+    sf::Vector2f size;
+    sf::Color color;
+    std::string name;
+    std::string filename;
+    float radius;
+    input >> position >> name;
+
+}
 
 #endif
