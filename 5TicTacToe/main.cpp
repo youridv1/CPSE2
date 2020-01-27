d710b8907bf4375518e302890e3243ec018884ab
Youri <youridv1@gmail.com>
2020-01-27 14:53:56 +0100

    done

--- 5TicTacToe/main.cpp
+++ 5TicTacToe/main.cpp
@@ -1,11 +1,14 @@
 #include "game.hpp"
 #include "cli.hpp"
 #include "gui.hpp"
+#include "string"
 
-#define GRAPHICAL false
-
-int main(){
-    if(GRAPHICAL){
+int main(int argc, char *argv[]){
+    std::string argument = "default";
+    if(argc == 2){
+        argument = argv[1];
+    }
+    if(argument == "graphical"){
         sf::RenderWindow window{sf::VideoMode{700, 655}, "Tic Tac Toe"};
         gui gui{window};
         TicTacToe game( gui );
